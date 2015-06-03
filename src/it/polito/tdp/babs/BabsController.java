bd69dcd1843d6d9f23cec030f70371af9ef690eb
fcorno <fulvio.corno@polito.it>
2015-06-03 10:53:27 +0200

    Progetto "BASE" per la simulazione d'esame

---
+++ src/it/polito/tdp/babs/BabsController.java
@@ -0,0 +1,30 @@
+package it.polito.tdp.babs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+
+public class BabsController {
+
+    @FXML
+    private Slider sliderK;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private DatePicker pickData;
+
+    @FXML
+    void doContaTrip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSimula(ActionEvent event) {
+
+    }
+
+}
